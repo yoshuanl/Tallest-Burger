c77f4eab9441acf81063340cdc53c9f1e50f5a75
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-09-10 21:18:08 -0700

    [feat] navigating to the checkout page using Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
+          <Route path="/" exact component={BurgerBuilder}/>
+          <Route path="/checkout" component={Checkout}/>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -84,33 +84,34 @@ class BurgerBuilder extends Component {
     }
 
     continuePurchaseHandler = () => {
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Scully',
-                address: {
-                    street: 'test',
-                    zipCode: '12345',
-                    country: 'Taiwan'
-                },
-                email: 'test@usc.edu'
-            },
-            deliveryMethod: 'fastest'
-        }
-        // .json is important for Firebase
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({
-                    loading: false,
-                    purchasing: false})
-            })
-            .catch(error => {
-                this.setState({
-                    loading: false,
-                    purchasing: false})
-            });
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Scully',
+        //         address: {
+        //             street: 'test',
+        //             zipCode: '12345',
+        //             country: 'Taiwan'
+        //         },
+        //         email: 'test@usc.edu'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // // .json is important for Firebase
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false})
+        //     })
+        //     .catch(error => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false})
+        //     });
+        this.props.history.push("/checkout"); // one of the special props provided by the Router
     }
 
 
