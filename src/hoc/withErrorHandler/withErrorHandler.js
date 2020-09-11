1d81a8696868c49ff7c654be8c3cd80ba35060b5
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-09-10 17:48:29 -0700

    [feat] HOC global error handler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment} from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    // anonymous class
+    return class extends Component {
+        state = {
+            error: null
+        }
+        componentDidMount () {
+            // whenever a 
+            axios.interceptors.request.use(req => {
+                this.setState({error: null})
+                return req; // so the request can continue
+            })
+            // error is an object that contains error message
+            axios.interceptors.response.use(response => response, error => {
+                this.setState({error: error})
+            })
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null})
+        }
+        render () {
+            return (
+                <Fragment>
+                <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+                    {this.state.error ? this.state.error.message : null}
+                </Modal>
+                <WrappedComponent {...this.props}/>
+                </Fragment>
+            )
+        }
+    }
+};
+    
+export default withErrorHandler;
