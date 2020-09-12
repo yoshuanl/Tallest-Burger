ea8d6a1d28472ada7585b11f46b3729fb2d0eedc
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-09-12 00:02:23 -0700

    [feat] create form for user input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,15 +4,36 @@ import classes from './Input.css';
     
 const input = (props) => {
     let inputElement = null;
-    switch (props.inputType) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props} />;
-            break
+            inputElement = <input 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed} />;
+            break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />;
-            break
+            inputElement = <textarea 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select className={classes.InputElement} value={props.value} onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>{option.displayValue}</option>
+                    ))}
+                </select>
+            );
+            break;
         default:
-            inputElement = <input className={classes.InputElement} {...props} />;
+            inputElement = <input 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed} />;
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,14 +8,60 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input', // normal html tags name
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input', // normal html tags name
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-mail'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input', // normal html tags name
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input', // normal html tags name
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input', // normal html tags name
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP-Code'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select', // normal html tags name
+                elementConfig: {
+                    options: [{value: 'fastest', displayValue: 'Fastest'},
+                            {value: 'cheapest', displayValue: 'Cheapest'} ]
+                },
+                value: ''
+            }
         },
+
         loading: false
     }
+
     orderHandler = (event) => {
         event.preventDefault(); // the default behavior of a <form/> is to send the request and reload the page
         this.setState({loading: true});
@@ -45,14 +91,35 @@ class ContactData extends Component {
                 this.setState({loading: false});
             });
     }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        console.log("inputChangedHandler")
+        const updatedOrderForm = {...this.state.orderForm};
+        const updatedElement = {...this.state.orderForm[inputIdentifier]};
+        updatedElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedElement;
+
+        this.setState({orderForm: updatedOrderForm})
+    }
+
     render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
-                <Input inputtype="input" type="text" name="mail" placeholder="Your mail"/>
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code"/>
-                <Input inputtype="input" type="text" name="city" placeholder="City"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Street"/>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/> // formElement.id is the key, which is name, email, ...
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
