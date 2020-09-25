e91d658cda7fac32358e1cc4b68e893a87de2b59
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-09-24 18:18:24 -0700

    [feat] fetch order data from server with redux

--- src/store/reducer/order.js
+++ src/store/reducer/order.js
@@ -11,17 +11,17 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 purchased: false
-            }
+            };
         case actionTypes.PURCHASE_BURGER_START:
             return {
                 ...state,
                 loading: true
-            }
+            };
         case actionTypes.PURCHASE_BURGER_SUCCESS:
             const newOrder ={
                 ...action.orderData,
                 id: action.orderId
-            }
+            };
             return {
                 ...state,
                 loading: false,
@@ -32,6 +32,22 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 loading: false,
+            };
+        case actionTypes.FETCH_ORDER_START:
+            return {
+                ...state,
+                loading: true
+            };
+        case actionTypes.FETCH_ORDER_SUCCESS:
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            };
+        case actionTypes.FETCH_ORDER_FAIL:
+            return {
+                ...state,
+                loading: false
             };
         default:
             return state;
