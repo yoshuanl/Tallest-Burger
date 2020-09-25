487fdcc9e0d6275c3d145c805f9269725da0ec55
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2020-09-25 16:48:35 -0700

    [feat] store token locally

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        token: token,
+        userId: userId
     };
 };
 
@@ -40,11 +41,11 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
         .then(response => {
             console.log(response);
-            dispatch(authSuccess(response.data));
+            dispatch(authSuccess(response.data.idToken, response.data.localId));
         })
-        .catch(err => {
-            console.log(err);
-            dispatch(authFail());
+        .catch(error => {
+            console.log(error);
+            dispatch(authFail(error));
         })
     }
 } 
---
+++ src/store/reducer/auth.js
@@ -0,0 +1,34 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false
+};
+
+const authStart = (state) => {
+    return updateObject(state, {error: null, loading: true});
+}
+
+const authSuccess = (state, action) => {
+    return updateObject(state, {token: action.token, userId: action.userId, error: null, loading: false});
+}
+
+const authFail = (state, action) => {
+    console.log(action.error);
+    return updateObject(state, {error: action.error, loading: false});
+}
+
+// set to initialState at the beginning otherwise it is undefined
+const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START: return authStart(state);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        default: return state;
+    }
+}
+
+export default reducer;
